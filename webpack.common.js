9040dcfd23cde2f378cafabe3da978da98562300
dev <devnetworksoft@gmail.com>
2024-10-27 16:15:14 -0500

    Initial Commit

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+/*const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');*/
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+    ],
+  }
+};
